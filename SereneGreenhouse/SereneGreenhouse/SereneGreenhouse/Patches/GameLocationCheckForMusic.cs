0ad7720331589a227fe5b9b0ccdf0cab253adfe6
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-02-07 21:53:52 -0600

    Potentially unneeded patches

---
+++ SereneGreenhouse/SereneGreenhouse/SereneGreenhouse/Patches/GameLocationCheckForMusic.cs
@@ -0,0 +1,46 @@
+﻿using Harmony;
+using StardewValley;
+using System.Reflection;
+using StardewModdingAPI;
+using Microsoft.Xna.Framework;
+using StardewValley.Menus;
+using System.Linq;
+using xTile.Dimensions;
+using xTile.Tiles;
+using System.Collections.Generic;
+
+namespace SereneGreenhouse.Patches
+{
+    [HarmonyPatch]
+    public class GameLocationCheckForMusic
+    {
+        private static IMonitor monitor = ModEntry.monitor;
+
+        internal static MethodInfo TargetMethod()
+        {
+            return AccessTools.Method(typeof(StardewValley.GameLocation), nameof(StardewValley.GameLocation.checkForMusic));
+        }
+
+        internal static bool Prefix(GameLocation __instance, GameTime time)
+        {
+            if (__instance.Name != "Greenhouse")
+            {
+                return true;
+            }
+
+            if (Game1.isMusicContextActiveButNotPlaying())
+            {
+                if (Game1.isRaining)
+                {
+                    Game1.changeMusicTrack("rain");
+                }
+                else if (!Game1.isDarkOut())
+                {
+                    Game1.changeMusicTrack("woodsTheme");
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SereneGreenhouse/SereneGreenhouse/SereneGreenhouse/Patches/GameLocationResetForPlayerEntry.cs
@@ -0,0 +1,41 @@
+﻿using Harmony;
+using StardewValley;
+using System.Reflection;
+using StardewModdingAPI;
+using Microsoft.Xna.Framework;
+using StardewValley.Menus;
+using System.Linq;
+using xTile.Dimensions;
+using xTile.Tiles;
+using System.Collections.Generic;
+
+namespace SereneGreenhouse.Patches
+{
+    [HarmonyPatch]
+    public class GameLocationResetForPlayerEntry
+    {
+        private static IMonitor monitor = ModEntry.monitor;
+
+        internal static MethodInfo TargetMethod()
+        {
+            return AccessTools.Method(typeof(StardewValley.GameLocation), nameof(StardewValley.GameLocation.resetForPlayerEntry));
+        }
+
+        internal static void Postfix(GameLocation __instance)
+        {
+            if (__instance.Name != "Greenhouse")
+            {
+                return;
+            }
+
+            if (Game1.isRaining)
+            {
+                Game1.changeMusicTrack("rain");
+            }
+            else if (Game1.timeOfDay < 1800)
+            {
+                Game1.changeMusicTrack("woodsTheme");
+            }
+        }
+    }
+}
